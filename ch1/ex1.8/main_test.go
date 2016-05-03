5f4f280c045efe3373dbf6856fb21d8b2d835696
Akash <akashkurdekar@gmail.com>
2016-05-03 19:13:05 +0100

    ex1.8

---
+++ ch1/ex1.8/main_test.go
@@ -0,0 +1,42 @@
+package main
+
+import "testing"
+import "github.com/stretchr/testify/assert"
+import "strings"
+
+func TestEnsureHttpPrefixDoesNothingIfPrefixAlreadyPresent(t *testing.T) {
+	var tests = []struct {
+		in  string
+		out string
+	}{
+		{"http://abc.com", "http://abc.com"},
+		{"http://www.ok.co.uk", "http://www.ok.co.uk"},
+		{"http://localhost", "http://localhost"},
+		{"http://192.168.0.1", "http://192.168.0.1"},
+	}
+	for _, tt := range tests {
+		assert.Equal(t, tt.out, ensureHTTP(tt.in))
+	}
+}
+
+func TestEnsureHttpPrefixAddsHttpPrefixIfNotPresent(t *testing.T) {
+	var tests = []struct {
+		in  string
+		out string
+	}{
+		{"abc.com", "http://abc.com"},
+		{"www.ok.co.uk", "http://www.ok.co.uk"},
+		{"localhost", "http://localhost"},
+		{"192.168.0.1", "http://192.168.0.1"},
+	}
+	for _, tt := range tests {
+		assert.Equal(t, tt.out, ensureHTTP(tt.in))
+	}
+}
+
+func ensureHTTP(url string) string {
+	if strings.HasPrefix(url, "http://") {
+		return url
+	}
+	return "http://" + url
+}
